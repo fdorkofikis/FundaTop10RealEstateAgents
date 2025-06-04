c7b9c23dc4aa9f030cb36992c227bfd6f46e7757
Fotis Dorkofikis <fotis@gmail.com>
2025-06-04 19:20:10 +0200

    Initial commit

---
+++ src/WebApi/Controllers/RealEstateAgentsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeApi.Controllers;
+
+[ApiController]
+[Route("api/makelaars")]
+public class RealEstateAgentsController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetTop10RealEstateAgents(CancellationToken cancellationToken)
+    {
+        return Ok();
+    }
+}
